44e4f4e7dea8edd6e9976d3c335b811164f25f5c
KKris7 <80973611+KKris7@users.noreply.github.com>
2022-06-08 13:14:39 +0300

    initial commit

---
+++ AppiumDesktopTests/AppiumDesktopTests/7ZipAppiumTest.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace AppiumDesktopTests
+{
+    public class _7ZipAppiumTest
+    {
+        private const string AppiumServerUrl = "http://[::1]:4723/wd/hub";
+        private const string App = @"C:\Program Files\7-Zip\7zFM.exe";
+
+        private WindowsDriver<WindowsElement> driver;
+        private WindowsDriver<WindowsElement> rootDriver;
+
+        private string workDir;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var desktopOptions = new AppiumOptions() { PlatformName = "Windows" };
+            desktopOptions.AddAdditionalCapability("app", "Root");
+            rootDriver = new WindowsDriver<WindowsElement>(new Uri(AppiumServerUrl), desktopOptions);
+
+
+            var options = new AppiumOptions() { PlatformName = "Windows" };
+            options.AddAdditionalCapability("app", App);
+            driver = new WindowsDriver<WindowsElement>(new Uri(AppiumServerUrl), options);
+
+            workDir = Directory.GetCurrentDirectory() + @"\workdir";
+            if (Directory.Exists(workDir))
+            {
+                Directory.Delete(workDir, true);
+            }
+            Directory.CreateDirectory(workDir);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            rootDriver.Quit();
+        }
+
+        [Test]
+        public void Test_7Zip_MaxCompression()
+        {
+            var locationTextBox = driver.FindElementByClassName("Edit");
+            locationTextBox.SendKeys(@"C:\Program Files\7-Zip\" + Keys.Enter);
+
+            var filesList = driver.FindElementByClassName("SysListView32");
+            filesList.SendKeys(Keys.Control + 'a');
+            driver.FindElementByName("Add").Click();
+
+            string archiveFileName = workDir + "\\" + DateTime.Now.Ticks + ".7z";
+
+            Thread.Sleep(500);
+
+            var archiveView = rootDriver.FindElementByName("Add to Archive");
+            var archiveNameTextBox = archiveView.FindElementByXPath("/Window/ComboBox/Edit[@Name='Archive:']");
+            archiveNameTextBox.SendKeys(archiveFileName);
+
+            var archiveFormat = archiveView.FindElementByXPath("/Window/ComboBox[@Name='Archive format:']");
+            archiveFormat.SendKeys(Keys.Home);
+
+            var compressionLevel = archiveView.FindElementByXPath("/Window/ComboBox[@Name='Compression level:']");
+            compressionLevel.SendKeys(Keys.End);
+
+            var compressionMethod = archiveView.FindElementByXPath("/Window/ComboBox[@Name='Compression method:']");
+            compressionMethod.SendKeys(Keys.Home);
+
+            var dictionarySize = archiveView.FindElementByXPath("/Window/ComboBox[@Name='Dictionary size:']");
+            dictionarySize.SendKeys(Keys.End);
+
+            var wordSize = archiveView.FindElementByXPath("/Window/ComboBox[@Name='Word size:']");
+            wordSize.SendKeys(Keys.End);
+
+            var okButton = archiveView.FindElementByXPath("/Window/Button[@Name='OK']");
+            okButton.Click();
+            Thread.Sleep(1000);
+
+            // extract
+            locationTextBox.SendKeys(archiveFileName + Keys.Enter);
+            var extractButton = driver.FindElementByName("Extract");
+            extractButton.Click();
+
+            var okButtonExtract = driver.FindElementByName("OK");
+            okButtonExtract.Click();
+
+            // needed time
+            Thread.Sleep(1500);
+
+            string extracted = workDir + @"\7zFM.exe";
+            FileAssert.AreEqual(App, extracted);
+        }
+    }
+}
---
+++ AppiumDesktopTests/AppiumDesktopTests/SummatorAppiumTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace AppiumDesktopTests
+{
+    public class SummatorAppiumTest
+    {
+        public const string AppiumServerUri = "http://[::1]:4723/wd/hub";
+        public const string SummatorAppPath = @"D:\SummatorDesktopApp.exe";
+        private WindowsDriver<WindowsElement> driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var appiumOptions = new AppiumOptions() { PlatformName = "Windows" };
+            appiumOptions.AddAdditionalCapability("app", SummatorAppPath);
+            driver = new WindowsDriver<WindowsElement>(new Uri(AppiumServerUri), appiumOptions);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void ValidTest()
+        {
+            var firstNumField = driver.FindElementByAccessibilityId("textBoxFirstNum");
+            var secondNumField = driver.FindElementByAccessibilityId("textBoxSecondNum");
+            var resultField = driver.FindElementByAccessibilityId("textBoxSum");
+            var calculateButton = driver.FindElementByAccessibilityId("buttonCalc");
+
+            firstNumField.Clear();
+            secondNumField.Clear();
+
+            firstNumField.Click();
+            firstNumField.SendKeys("12");
+            secondNumField.Click();
+            secondNumField.SendKeys("30");
+
+            calculateButton.Click();
+
+            var expectedResult = "42";
+
+            Assert.That(expectedResult, Is.EqualTo(resultField.Text));
+
+        }
+        [Test]
+        public void InvalidTest()
+        {
+            var firstNumField = driver.FindElementByAccessibilityId("textBoxFirstNum");
+            var secondNumField = driver.FindElementByAccessibilityId("textBoxSecondNum");
+            var resultField = driver.FindElementByAccessibilityId("textBoxSum");
+            var calculateButton = driver.FindElementByAccessibilityId("buttonCalc");
+
+            firstNumField.Clear();
+            secondNumField.Clear();
+
+            firstNumField.Click();
+            firstNumField.SendKeys("");
+            secondNumField.Click();
+            secondNumField.SendKeys("");
+
+            calculateButton.Click();
+
+            var expectedResult = "error";
+
+            Assert.That(expectedResult, Is.EqualTo(resultField.Text));
+
+        }
+    }
+}
